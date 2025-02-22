b53f6aaa41ebb40aba231b87c69ccdd3fadf9727
Santee <santee014@gmail.com>
2025-02-22 04:49:05 -0600

    Update fxmanifest.lua

--- restrictVehicles/fxmanifest.lua
+++ restrictVehicles/fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'SanteeDesigns'
 description 'Restricts vehicles based on Discord roles using zDiscord'
-version '1.1.1'
+version '1.1.0'
 
 server_scripts {
     '@zDiscord/**/client.lua', -- Ensure zDiscord is running
